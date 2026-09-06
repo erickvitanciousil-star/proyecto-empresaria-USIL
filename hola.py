737ca6e82afce9548dfb2fbe9d080017d6e58b26
Erick <erickvitanciousil@gmail.com>
2026-09-06 03:24:55 -0500

    Añadida secret_key a flet app

--- hola.py
+++ hola.py
@@ -1,12 +1,13 @@
 import flet as ft
-import flet_fastapi
 
 def main(page: ft.Page):
+    # Aquí va todo el código principal de tu interfaz
     page.title = "App Flet USIL"
     page.add(ft.Text("¡Hola! Tu aplicación en Flet ya está en línea correctamente.", size=20))
 
-# Crear la aplicación FastAPI asignada
-app = flet_fastapi.app(main, assets_dir="assets")
-
 if __name__ == "__main__":
-    ft.app(target=main, assets_dir="assets")
+    ft.app(
+        target=main, 
+        assets_dir="assets",
+        secret_key="mi_clave_secreta_flet"
+    )
