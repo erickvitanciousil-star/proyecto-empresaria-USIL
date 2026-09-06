7e0e615c72f76c53082162a0eb006a41beaf7b30
Erick <erickvitanciousil@gmail.com>
2026-09-06 02:54:40 -0500

    Adaptado hola.py para FastAPI

--- hola.py
+++ hola.py
@@ -1,1 +1,12 @@
-"¡Hola Mundo! Mi primer código en Python está listo."
+import flet as ft
+from flet_fastapi import app as flet_fastapi_app
+
+def main(page: ft.Page):
+    # Tu código actual de la aplicación va aquí adentro
+    pass
+
+# Esta línea permite que Render/uvicorn ejecuten la app en la nube
+app = flet_fastapi_app(main)
+
+if __name__ == "__main__":
+    ft.app(target=main)
