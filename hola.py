7164a14622672fcddde50c4227f6f2a702aada9d
Erick <erickvitanciousil@gmail.com>
2026-09-06 03:15:15 -0500

    Montaje correcto de flet_fastapi

--- hola.py
+++ hola.py
@@ -2,10 +2,10 @@ import flet as ft
 import flet_fastapi
 
 def main(page: ft.Page):
-    # Tu código actual de la app
-    page.add(ft.Text("¡App Flet cargada con éxito!"))
+    page.title = "App Flet USIL"
+    page.add(ft.Text("¡Hola! Tu aplicación en Flet ya está en línea correctamente.", size=20))
 
-# Monta la aplicación con soporte para assets/imágenes
+# Crear la aplicación FastAPI asignada
 app = flet_fastapi.app(main, assets_dir="assets")
 
 if __name__ == "__main__":
