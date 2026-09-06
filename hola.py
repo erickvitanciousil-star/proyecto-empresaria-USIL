dd208a27ad7bea0f8b2c6893af3119c762f2c915
Erick <erickvitanciousil@gmail.com>
2026-09-06 03:35:17 -0500

    Migrado a uvicorn flet-fastapi

--- hola.py
+++ hola.py
@@ -1,13 +1,11 @@
 import flet as ft
+from flet_fastapi import app as flet_fastapi_app
 
 def main(page: ft.Page):
-    # Aquí va todo el código principal de tu interfaz
     page.title = "App Flet USIL"
     page.add(ft.Text("¡Hola! Tu aplicación en Flet ya está en línea correctamente.", size=20))
 
+app = flet_fastapi_app(main, assets_dir="assets")
+
 if __name__ == "__main__":
-    ft.app(
-        target=main, 
-        assets_dir="assets",
-        secret_key="mi_clave_secreta_flet"
-    )
+    ft.app(target=main)
