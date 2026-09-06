78641127a7983bf772879e11b139b13e1444e4a3
Erick <erickvitanciousil@gmail.com>
2026-09-06 10:53:40 -0500

    Cambiado main a funcion sincrona

--- hola.py
+++ hola.py
@@ -1,13 +1,9 @@
 import flet as ft
 import flet_fastapi
 
-async def main(page: ft.Page):
+def main(page: ft.Page):
     page.title = "Desayunos Express - USIL"
     page.add(ft.Text("¡Aplicación Desayunos Express en línea!", size=24, weight="bold"))
 
-# Crear la app web de Flet compatible con uvicorn
+# flet_fastapi requiere una función síncrona como target
 app = flet_fastapi.app(main)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("hola:app", host="0.0.0.0", port=8000)
